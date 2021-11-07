e223bdb46d6047ca3daef361aca6584e62c19dc3
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-07 17:45:46 +0200

    Added API key & base URL

---
+++ src/js/api/api-data.js
@@ -0,0 +1,5 @@
+export const API_KEY = 'c686fd0cacddd11fd41af6fdd62727e4';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+
+/*  Пример API-запроса https://api.themoviedb.org/3/movie/550?api_key=c686fd0cacddd11fd41af6fdd62727e4 ,
+подробнее в документации API */
