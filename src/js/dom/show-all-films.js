8a29ea907c49bac061168a12fe3ad7698b9bd756
Kateryna <koljuchka93@gmail.com>
2021-11-10 10:15:06 +0200

    add refreshPage to home btn and logo

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -13,3 +13,18 @@ function createMarkup(data) {
 }
 
 showAllFilms(homePage);
+
+//FT-02,13 By clicking on the logo/"Home" btn, the main page is render.
+function refreshPage() {
+  document.location.reload();
+
+  logo.addEventListener('click', refreshPage);
+  homebtn.addEventListener('click', refreshPage);
+}
+
+// const startPage = showAllFilms(homePage)
+// btnGoHome.addEventListener('click', startPage)
+
+// const startPage = showAllFilms(homePage)
+
+// logo.addEventListener('click', startPage)
