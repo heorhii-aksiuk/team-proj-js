271205de2db4f0c134f20bad4cc49bbd7f7d8d45
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-11 01:46:44 +0200

    Change page by number button and refactoring

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -32,34 +32,49 @@ showAllFilms(homePage);
 nextPageBtnEl.addEventListener('click', nextPage)
 previousPageBtnEl.addEventListener('click', previousPage);
 
+function pageChoose(button) {
+  button.addEventListener('click', changePage);
+
+  function changePage() {
+    currentPage = +button.textContent;
+    buttonsTextContentChange();
+    showAllFilms(currentPage);
+  }
+}
+
+pageChoose(plusTwoBtnEl);
+pageChoose(plusOneBtnEl);
+pageChoose(minusOneBtnEl);
+pageChoose(minusTwoBtnEl);
 
 function nextPage() {
   currentPage += 1;
-  plusTwoBtnEl.textContent = currentPage + 2;
-  plusOneBtnEl.textContent = currentPage + 1;
-  currentBtnEl.textContent = currentPage;
-  minusOneBtnEl.textContent = currentPage - 1;
-  if (currentPage > 2) minusTwoBtnEl.textContent = currentPage - 2;
+  buttonsTextContentChange();
   showAllFilms(currentPage);
 }
 
 function previousPage() {
+  if (currentPage <= 1) return
   currentPage -= 1;
+  buttonsTextContentChange();
+  showAllFilms(currentPage);
+}
+
+function buttonsTextContentChange() {
   plusTwoBtnEl.textContent = currentPage + 2;
   plusOneBtnEl.textContent = currentPage + 1;
   currentBtnEl.textContent = currentPage;
-  
-  if (currentPage < 2) {
+
+  if (currentPage === 1) {
     minusOneBtnEl.textContent = '';
+    minusTwoBtnEl.textContent = '';
+    
+  } else if (currentPage === 2) {
+    minusOneBtnEl.textContent = currentPage - 1;
+    minusTwoBtnEl.textContent = '';
+    
   } else {
     minusOneBtnEl.textContent = currentPage - 1;
-  }
-  
-  if (currentPage < 3) {
-    minusTwoBtnEl.textContent = '';
-  } else {
     minusTwoBtnEl.textContent = currentPage - 2;
   }
-
-  showAllFilms(currentPage);
 }
