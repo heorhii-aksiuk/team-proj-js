9d1307aafc87996aed38edfca8dca1ff57c7dd44
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-11 00:01:25 +0200

    MVP Next page function

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -14,6 +14,10 @@ import {
 import filmCard from '../../templates/film-card.hbs';
 
 const homePage = 1;
+const totalPage = 1000;
+let currentPage = homePage;
+plusTwoBtnEl.textContent = currentPage + 2;
+plusOneBtnEl.textContent = currentPage + 1;
 
 function showAllFilms(page) {
   fetchAllFilms(page).then(createMarkup);
@@ -24,3 +28,15 @@ export function createMarkup(data) {
 }
 
 showAllFilms(homePage);
+
+nextPageBtnEl.addEventListener('click', nextPage)
+
+function nextPage() {
+  currentPage += 1;
+  plusTwoBtnEl.textContent = currentPage + 2;
+  plusOneBtnEl.textContent = currentPage + 1;
+  currentBtnEl.textContent = currentPage;
+  minusOneBtnEl.textContent = currentPage - 1;
+  if (currentPage > 2) minusTwoBtnEl.textContent = currentPage - 2;
+  showAllFilms(currentPage);
+}
