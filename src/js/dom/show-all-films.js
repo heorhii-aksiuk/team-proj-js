e56e71771313da3d9d80cb4b7f3a7e11fbe24e14
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-11 22:33:13 +0200

    MVP all pagination

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -1,23 +1,57 @@
 import { fetchAllFilms } from '../api/fetch-all-films';
 import {
   mainCardListEl,
+  paginationListEl,
   previousPageBtnEl,
   firstPageBtnEl,
+  previousDotsLiEl,
   minusTwoBtnEl,
   minusOneBtnEl,
   currentBtnEl,
   plusOneBtnEl,
   plusTwoBtnEl,
+  nextDotsLiEl,
   lastPageBtnEl,
   nextPageBtnEl,
 } from '../refs';
 import filmCard from '../../templates/film-card.hbs';
 
 const homePage = 1;
-const totalPage = 1000;
+let totalPage = 1000;
 let currentPage = homePage;
+
+if (totalPage > 3) {
+  nextDotsLiEl.classList.remove('content-hidden');
+  lastPageBtnEl.textContent = totalPage;
+} else {
+  nextDotsLiEl.classList.add('content-hidden');
+  lastPageBtnEl.textContent = '';
+}
+
 plusTwoBtnEl.textContent = currentPage + 2;
 plusOneBtnEl.textContent = currentPage + 1;
+
+function showOrHideDots() {
+
+  let toStartPageLeft = currentPage - 1;
+  let toEndPageLeft = totalPage - currentPage;
+
+  if (toEndPageLeft < 3) {
+    nextDotsLiEl.classList.add('content-hidden');
+    lastPageBtnEl.textContent = '';
+  } else {
+    nextDotsLiEl.classList.remove('content-hidden');
+    lastPageBtnEl.textContent = totalPage;
+  }
+
+  if (toStartPageLeft < 3) {
+    previousDotsLiEl.classList.add('content-hidden');
+    firstPageBtnEl.textContent = '';
+  } else {
+    previousDotsLiEl.classList.remove('content-hidden');
+    firstPageBtnEl.textContent = '1';
+  }
+}
 
 function showAllFilms(page) {
   fetchAllFilms(page).then(createMarkup);
@@ -31,50 +65,63 @@ showAllFilms(homePage);
 
 nextPageBtnEl.addEventListener('click', nextPage)
 previousPageBtnEl.addEventListener('click', previousPage);
+paginationListEl.addEventListener('click', changePage);
 
-function pageChoose(button) {
-  button.addEventListener('click', changePage);
-
-  function changePage() {
-    currentPage = +button.textContent;
-    buttonsTextContentChange();
+function changePage(event) {
+  if (+event.target.textContent > 0)
+    currentPage = +event.target.textContent;
+  if (currentPage > totalPage) return;
+    buttonsContentChange();
     showAllFilms(currentPage);
-  }
 }
 
-pageChoose(plusTwoBtnEl);
-pageChoose(plusOneBtnEl);
-pageChoose(minusOneBtnEl);
-pageChoose(minusTwoBtnEl);
-
 function nextPage() {
+  if (currentPage === totalPage) return;
   currentPage += 1;
-  buttonsTextContentChange();
+  buttonsContentChange();
   showAllFilms(currentPage);
 }
 
 function previousPage() {
   if (currentPage <= 1) return
   currentPage -= 1;
-  buttonsTextContentChange();
+  buttonsContentChange();
   showAllFilms(currentPage);
 }
 
-function buttonsTextContentChange() {
-  plusTwoBtnEl.textContent = currentPage + 2;
-  plusOneBtnEl.textContent = currentPage + 1;
-  currentBtnEl.textContent = currentPage;
-
-  if (currentPage === 1) {
-    minusOneBtnEl.textContent = '';
-    minusTwoBtnEl.textContent = '';
-    
-  } else if (currentPage === 2) {
-    minusOneBtnEl.textContent = currentPage - 1;
-    minusTwoBtnEl.textContent = '';
-    
-  } else {
+function buttonsContentChange() {
+  if (currentPage > totalPage) {
+    currentBtnEl.textContent = currentPage;
     minusOneBtnEl.textContent = currentPage - 1;
     minusTwoBtnEl.textContent = currentPage - 2;
+  } else if (currentPage === totalPage) {
+    plusTwoBtnEl.textContent = '';
+    plusOneBtnEl.textContent = '';
+    currentBtnEl.textContent = currentPage;
+    minusOneBtnEl.textContent = currentPage - 1;
+    minusTwoBtnEl.textContent = currentPage - 2;
+    } else if (currentPage + 1 === totalPage) {
+    plusTwoBtnEl.textContent = '';
+    plusOneBtnEl.textContent = currentPage + 1;
+    currentBtnEl.textContent = currentPage;
+    minusOneBtnEl.textContent = currentPage - 1;
+    minusTwoBtnEl.textContent = currentPage - 2;
+  } else {
+    plusTwoBtnEl.textContent = currentPage + 2;
+    plusOneBtnEl.textContent = currentPage + 1;
+    currentBtnEl.textContent = currentPage;
+
+    if (currentPage === 1) {
+      minusOneBtnEl.textContent = '';
+      minusTwoBtnEl.textContent = '';
+    } else if (currentPage === 2) {
+      minusOneBtnEl.textContent = currentPage - 1;
+      minusTwoBtnEl.textContent = '';
+    } else {
+      minusOneBtnEl.textContent = currentPage - 1;
+      minusTwoBtnEl.textContent = currentPage - 2;
+    }
   }
+
+  showOrHideDots();
 }
