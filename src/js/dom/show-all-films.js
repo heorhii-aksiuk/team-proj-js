4ec231698f69f96e2d8b50749028a3d9773401e0
Yehor Tuz <tuzegor@gmail.com>
2021-11-11 17:02:16 +0200

    Убрал console.log

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -10,12 +10,10 @@ function showAllFilms(page) {
 
 export function createMarkup(data) {
   const {films, genres} = data;
-  console.log(data);
   const filmsWithGenre = films.map(film => ({
     ...film,
     genres: genres.filter(genre => film.genre_ids.includes(genre.id))
   }));
-  console.log(filmsWithGenre);
   mainCardListEl.innerHTML = filmCard(filmsWithGenre);
   
 }
