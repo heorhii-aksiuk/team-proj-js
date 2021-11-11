ec20c125d53d2e95c26f7751d26c4047d8d14817
Ivan Suprun <bender.slam@gmail.com>
2021-11-12 00:07:30 +0200

    Исправил названия фильмов, добавил рейтинг и отображение даты релиза (пока что с багом в дате)

--- src/js/api/api-data.js
+++ src/js/api/api-data.js
@@ -1,5 +1,6 @@
 export const API_KEY = 'c686fd0cacddd11fd41af6fdd62727e4';
 export const BASE_URL = 'https://api.themoviedb.org/3';
+export const ID_URL = `${BASE_URL}/movie/`;
 
 /*  Пример API-запроса https://api.themoviedb.org/3/movie/550?api_key=c686fd0cacddd11fd41af6fdd62727e4 ,
 подробнее в документации API */
