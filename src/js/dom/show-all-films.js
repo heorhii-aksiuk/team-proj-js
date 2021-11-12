7c28d131f21653d9ef23b449689abb4a854b3327
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-12 19:02:56 +0200

    Bugfix and refactoring

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -16,24 +16,38 @@ import {
 } from '../refs';
 import filmCard from '../../templates/film-card.hbs';
 
+
+
+
 const homePage = 1;
 let totalPage = 1000;
 let currentPage = homePage;
 
-minusOneBtnEl.classList.add('content-hidden');
-minusTwoBtnEl.classList.add('content-hidden');
-firstPageBtnEl.classList.add('content-hidden');
+// function pagination(totalPage) {
+//   startPaginationSetup();
 
-if (totalPage > 3) {
-  nextDotsLiEl.classList.remove('content-hidden');
-  lastPageBtnEl.textContent = totalPage;
-} else {
-  nextDotsLiEl.classList.add('content-hidden');
-  lastPageBtnEl.textContent = '';
+
+// }
+
+
+function startPaginationSetup() {
+  minusOneBtnEl.classList.add('content-hidden');
+  minusTwoBtnEl.classList.add('content-hidden');
+  firstPageBtnEl.classList.add('content-hidden');
+
+  if (totalPage > 3) {
+    nextDotsLiEl.classList.remove('content-hidden');
+    lastPageBtnEl.textContent = totalPage;
+  } else {
+    nextDotsLiEl.classList.add('content-hidden');
+    lastPageBtnEl.textContent = '';
+  }
+
+  plusTwoBtnEl.textContent = currentPage + 2;
+  plusOneBtnEl.textContent = currentPage + 1;
 }
 
-plusTwoBtnEl.textContent = currentPage + 2;
-plusOneBtnEl.textContent = currentPage + 1;
+startPaginationSetup()
 
 function showOrHideDots() {
 
@@ -79,34 +93,32 @@ function changePage(event) {
   if (+event.target.textContent > 0)
     currentPage = +event.target.textContent;
   if (currentPage > totalPage) return;
-    buttonsContentChange();
+    buttonsShow();
     showAllFilms(currentPage);
 }
 
 function nextPage() {
   if (currentPage === totalPage) return;
   currentPage += 1;
-  buttonsContentChange();
+  buttonsShow();
   showAllFilms(currentPage);
 }
 
 function previousPage() {
   if (currentPage <= 1) return
   currentPage -= 1;
-  buttonsContentChange();
+  buttonsShow();
   showAllFilms(currentPage);
 }
 
-function buttonsContentChange() {
-  minusTwoBtnEl.textContent = currentPage - 2;
-  minusOneBtnEl.textContent = currentPage - 1;
-  currentBtnEl.textContent = currentPage;
-  plusOneBtnEl.textContent = currentPage + 1;
-  plusTwoBtnEl.textContent = currentPage + 2;
+function buttonsShow() {
+  changePageNumbers();
 
   if (currentPage >= totalPage) {
     plusTwoBtnEl.classList.add('content-hidden');
     plusOneBtnEl.classList.add('content-hidden');
+    minusOneBtnEl.classList.remove('content-hidden');
+    minusTwoBtnEl.classList.remove('content-hidden');
 
   } else if (currentPage + 1 === totalPage) {
     plusTwoBtnEl.classList.add('content-hidden');
@@ -131,4 +143,12 @@ function buttonsContentChange() {
   }
 
   showOrHideDots();
+}
+
+function changePageNumbers() {
+  minusTwoBtnEl.textContent = currentPage - 2;
+  minusOneBtnEl.textContent = currentPage - 1;
+  currentBtnEl.textContent = currentPage;
+  plusOneBtnEl.textContent = currentPage + 1;
+  plusTwoBtnEl.textContent = currentPage + 2;
 }
