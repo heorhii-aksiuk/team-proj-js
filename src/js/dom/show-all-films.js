f3540d842394ce0f1da50f19e3ba4c4cb7b83312
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-13 13:47:24 +0200

    Bugfix

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -16,19 +16,17 @@ import {
 } from '../refs';
 import filmCard from '../../templates/film-card.hbs';
 
-
-
-
+const pages = 3;
 const homePage = 1;
-let totalPage = 1000;
+let totalPage = 1;
 let currentPage = homePage;
 
-// function pagination(totalPage) {
-//   startPaginationSetup();
+function pagination(totalPageInFetch) {
+  totalPage = totalPageInFetch;
+  return totalPage;
+}
 
-
-// }
-
+pagination(pages);
 
 function startPaginationSetup() {
   minusOneBtnEl.classList.add('content-hidden');
@@ -42,18 +40,21 @@ function startPaginationSetup() {
     nextDotsLiEl.classList.add('content-hidden');
     lastPageBtnEl.textContent = '';
   }
-
+  
   plusTwoBtnEl.textContent = currentPage + 2;
   plusOneBtnEl.textContent = currentPage + 1;
+  
+  if (totalPage < 2) plusOneBtnEl.classList.add('content-hidden');
+  if (totalPage < 3) plusTwoBtnEl.classList.add('content-hidden');
 }
 
 startPaginationSetup()
 
 function showOrHideDots() {
-
+  
   let toStartPageLeft = currentPage - 1;
   let toEndPageLeft = totalPage - currentPage;
-
+  
   if (toEndPageLeft < 3) {
     nextDotsLiEl.classList.add('content-hidden');
     lastPageBtnEl.classList.add('content-hidden');
@@ -63,7 +64,7 @@ function showOrHideDots() {
     lastPageBtnEl.classList.remove('content-hidden');
     lastPageBtnEl.textContent = totalPage;
   }
-
+  
   if (toStartPageLeft < 3) {
     previousDotsLiEl.classList.add('content-hidden');
     firstPageBtnEl.classList.add('content-hidden');
@@ -73,17 +74,13 @@ function showOrHideDots() {
     firstPageBtnEl.classList.remove('content-hidden');
     firstPageBtnEl.textContent = '1';
   }
+  
+  if (minusOneBtnEl.textContent < 1) minusOneBtnEl.classList.add('content-hidden');
+  if (minusTwoBtnEl.textContent < 1) minusTwoBtnEl.classList.add('content-hidden');
+  if (minusOneBtnEl.textContent > 0) minusOneBtnEl.classList.remove('content-hidden');
+  if (minusTwoBtnEl.textContent > 0) minusTwoBtnEl.classList.remove('content-hidden');
+  
 }
-
-function showAllFilms(page) {
-  fetchAllFilms(page).then(createMarkup);
-}
-
-export function createMarkup(data) {
-  mainCardListEl.innerHTML = filmCard(data);
-}
-
-showAllFilms(homePage);
 
 nextPageBtnEl.addEventListener('click', nextPage)
 previousPageBtnEl.addEventListener('click', previousPage);
@@ -91,10 +88,10 @@ paginationListEl.addEventListener('click', changePage);
 
 function changePage(event) {
   if (+event.target.textContent > 0)
-    currentPage = +event.target.textContent;
+  currentPage = +event.target.textContent;
   if (currentPage > totalPage) return;
-    buttonsShow();
-    showAllFilms(currentPage);
+  buttonsShow();
+  showAllFilms(currentPage);
 }
 
 function nextPage() {
@@ -113,25 +110,25 @@ function previousPage() {
 
 function buttonsShow() {
   changePageNumbers();
-
+  
   if (currentPage >= totalPage) {
     plusTwoBtnEl.classList.add('content-hidden');
     plusOneBtnEl.classList.add('content-hidden');
     minusOneBtnEl.classList.remove('content-hidden');
     minusTwoBtnEl.classList.remove('content-hidden');
-
+    
   } else if (currentPage + 1 === totalPage) {
     plusTwoBtnEl.classList.add('content-hidden');
     plusOneBtnEl.classList.remove('content-hidden');
-
+    
   } else {
     plusTwoBtnEl.classList.remove('content-hidden');
     plusOneBtnEl.classList.remove('content-hidden');
-
+    
     if (currentPage === 1) {
       minusOneBtnEl.classList.add('content-hidden');
       minusTwoBtnEl.classList.add('content-hidden');
-
+      
     } else if (currentPage === 2) {
       minusOneBtnEl.classList.remove('content-hidden');
       minusTwoBtnEl.classList.add('content-hidden');
@@ -141,7 +138,6 @@ function buttonsShow() {
       minusTwoBtnEl.classList.remove('content-hidden');
     }
   }
-
   showOrHideDots();
 }
 
@@ -152,3 +148,14 @@ function changePageNumbers() {
   plusOneBtnEl.textContent = currentPage + 1;
   plusTwoBtnEl.textContent = currentPage + 2;
 }
+
+
+function showAllFilms(page) {
+  fetchAllFilms(page).then(createMarkup);
+}
+
+export function createMarkup(data) {
+  mainCardListEl.innerHTML = filmCard(data);
+}
+
+showAllFilms(homePage);
