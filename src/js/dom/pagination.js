64c44421aecfad8e6f9d22e30a0985ddc67ed3e6
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-13 21:10:51 +0200

    Refactoring, improve and universality

--- src/js/dom/pagination.js
+++ src/js/dom/pagination.js
@@ -14,63 +14,72 @@ import {
 } from '../refs';
 
 import { showAllFilms } from './show-all-films'
-
-/* Start setup */
-
-let totalPages = 4;
+// Настройки для отрисовки фильмов по умолчанию
+let totalPages = 1000;
 const homePage = 1;
 let lastPage = 1;
 let currentPage = homePage;
 
-function pagination(totalPages) {
+// Функция, которая будет подключать пагинацию к страничке на которой находится пользователь
+/* // В условия проверка на какой страничке находится пользователь
+if () {
+// функция для пагинации поискового запроса
+} else if () {
+  // функция для пагинации watched
+} else if (){
+// функция для пагинации queue
+} else {
+// функция для пагинации главной странички
+  pagination(totalPages, showAllFilms);
+} */
+
+// Это вызов функции пагинации для главной странички, принимает количество страниц, и функцию для отрисовки всего в дом
+// Функция по умолчанию, после реальизации проверки сверху, удалить от сюда
+pagination(totalPages, showAllFilms);
+  
+// Реализация функционала
+
+function pagination(totalPages, showFilmsFunction) {
   lastPage = totalPages;
-  return lastPage;
-}
+  startPaginationSetup();
 
-/* Main */
+  paginationListEl.addEventListener('click', changePage);
+  previousPageBtnEl.addEventListener('click', previousPage);
+  nextPageBtnEl.addEventListener('click', nextPage);
 
-pagination(totalPages);
+  function changePage(event) {
+    if (+event.target.textContent > 0) currentPage = +event.target.textContent;
+    if (currentPage > lastPage) return;
+    buttonsShow();
+    showFilmsFunction(currentPage);
+  }
 
-startPaginationSetup();
+  function nextPage() {
+    if (currentPage === lastPage) return;
+    currentPage += 1;
+    buttonsShow();
+    showFilmsFunction(currentPage);
+  }
 
-paginationListEl.addEventListener('click', changePage);
-previousPageBtnEl.addEventListener('click', previousPage);
-nextPageBtnEl.addEventListener('click', nextPage);
-
-/* Functions */
-
-function changePage(event) {
-  if (+event.target.textContent > 0) currentPage = +event.target.textContent;
-  if (currentPage > lastPage) return;
-  buttonsShow();
-  showAllFilms(currentPage);
-}
-
-function nextPage() {
-  if (currentPage === lastPage) return;
-  currentPage += 1;
-  buttonsShow();
-  showAllFilms(currentPage);
-}
-
-function previousPage() {
-  if (currentPage <= 1) return;
-  currentPage -= 1;
-  buttonsShow();
-  showAllFilms(currentPage);
+  function previousPage() {
+    if (currentPage <= 1) return;
+    currentPage -= 1;
+    buttonsShow();
+    showFilmsFunction(currentPage);
+  }
 }
 
 function startPaginationSetup() {
   firstPageBtnEl.textContent = homePage;
   lastPageBtnEl.textContent = lastPage;
   changePageNumbers();
-  
+
   firstPageBtnEl.classList.add('content-hidden');
   minusOneBtnEl.classList.add('content-hidden');
   minusTwoBtnEl.classList.add('content-hidden');
   nextDotsLiEl.classList.add('content-hidden');
   lastPageBtnEl.classList.add('content-hidden');
-  
+
   if (lastPage <= 1) {
     plusOneBtnEl.classList.add('content-hidden');
   } else if (lastPage === 2) {
@@ -86,7 +95,7 @@ function startPaginationSetup() {
 
 function showFirstAndLastPages() {
   let toLastPageLeft = lastPage - currentPage;
-  
+
   if (toLastPageLeft < 3) {
     nextDotsLiEl.classList.add('content-hidden');
     lastPageBtnEl.classList.add('content-hidden');
@@ -94,7 +103,7 @@ function showFirstAndLastPages() {
     nextDotsLiEl.classList.remove('content-hidden');
     lastPageBtnEl.classList.remove('content-hidden');
   }
-  
+
   if (currentPage < 4) {
     previousDotsLiEl.classList.add('content-hidden');
     firstPageBtnEl.classList.add('content-hidden');
@@ -108,7 +117,7 @@ function buttonsShow() {
   changePageNumbers();
   showFirstAndLastPages();
   hiddeMinusButtons();
-  
+
   if (currentPage >= lastPage) {
     plusTwoBtnEl.classList.add('content-hidden');
     plusOneBtnEl.classList.add('content-hidden');
@@ -118,7 +127,7 @@ function buttonsShow() {
   } else {
     plusTwoBtnEl.classList.remove('content-hidden');
     plusOneBtnEl.classList.remove('content-hidden');
-    
+
     if (currentPage === 1) {
       minusOneBtnEl.classList.add('content-hidden');
       minusTwoBtnEl.classList.add('content-hidden');
@@ -146,4 +155,3 @@ function hiddeMinusButtons() {
   if (minusTwoBtnEl.textContent > 0) minusTwoBtnEl.classList.remove('content-hidden');
   if (minusOneBtnEl.textContent > 0) minusOneBtnEl.classList.remove('content-hidden');
 }
-
