c843aa81723d5b442cc20b07b3af82e93df2c279
Yehor Tuz <tuzegor@gmail.com>
2021-11-14 09:19:39 +0200

    Оптимизация кода header.
    немного поправил код в js -8 строк.
    отформатировал ком в хедер цсс.
    добавил пару классов в хедер цсс.
    добавлен перезапуск страницы при нажатии на хоум. (но тут еще можно подумать)

--- src/js/dom/home-library-page-switch.js
+++ src/js/dom/home-library-page-switch.js
@@ -6,17 +6,10 @@ libraryBtnEl.addEventListener('click', showLibraryPage);
 function showHomePage() {
   headerEl.classList.add('header-home');
   headerEl.classList.remove('header-library');
-  searchFormEl.classList.remove('hidden');
-  libraryMenuEl.classList.add('hidden');
-  homeBtnEl.parentNode.classList.add('active-page');
-  libraryBtnEl.parentNode.classList.remove('active-page');
+  window.location = '/';  
 }
 
 function showLibraryPage() {
   headerEl.classList.add('header-library');
   headerEl.classList.remove('header-home');
-  searchFormEl.classList.add('hidden');
-  libraryMenuEl.classList.remove('hidden');
-  libraryBtnEl.parentNode.classList.add('active-page');
-  homeBtnEl.parentNode.classList.remove('active-page');
 }
