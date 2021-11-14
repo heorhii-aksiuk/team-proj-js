e7bdfa02b772a16b9a75974c24889de489859d38
Yehor Tuz <tuzegor@gmail.com>
2021-11-14 20:33:02 +0200

    Правки

--- src/js/dom/modal-film-info.js
+++ src/js/dom/modal-film-info.js
@@ -1,31 +1,27 @@
 import { renderParamsCard } from '../api/fetch-info';
-// import filmCard from '../../templates/film-info.hbs'
+import filmCard from '../../templates/film-info.hbs';
 
-const modal = document.querySelector('.modal-backdrop')
-const modalContainer = document.querySelector('.right-side')
+const modal = document.querySelector('.modal-backdrop');
+const modalContainer = document.querySelector('.modal-wrap');
 const imagesRef = document.querySelector('.js-collection__list');
 
 let id = 'id';
 
-
 function renderMovieSeorchParam(id) {
   renderParamsCard(id)
-      .then(data => {
-          console.log(data)
-        // modalContainer.innerHTML = filmCard(data) 
-    // renderParamCard(data);
+    .then(data => {
+      modalContainer.innerHTML = filmCard(data);
     })
     .catch(() => {});
 }
 
-  imagesRef.addEventListener('click', e => {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
-      return;
-      }
-      
+imagesRef.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
 
-      id = e.target.dataset.id;
-      renderMovieSeorchParam(id);
-      modal.classList.remove('hidden')
-  });
+  id = e.target.dataset.id;
+  renderMovieSeorchParam(id);
+  modal.classList.remove('hidden');
+});
