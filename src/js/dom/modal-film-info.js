e786d57833e2828d24f5bd3490a0ed51d47ebb85
Iurii Papushoi <ones.man.it@gmail.com>
2021-11-15 21:45:23 +0200

    add closing modalframe (esc,btn,backdrop) mvp1

--- src/js/dom/modal-film-info.js
+++ src/js/dom/modal-film-info.js
@@ -1,21 +1,19 @@
 import { renderParamsCard } from '../api/fetch-info';
 import filmCard from '../../templates/film-info.hbs';
-
-const modal = document.querySelector('.backdrop');
-const modalContainer = document.querySelector('.modal-wrap');
-const imagesRef = document.querySelector('.js-collection__list');
+import { backdrop, modalFrame, mainCardListEl, bodyEl } from '../refs';
 
 let id = 'id';
 
 function renderMovieSearchParam(id) {
   renderParamsCard(id)
     .then(data => {
-      modalContainer.innerHTML = filmCard(data);
+      console.log('data:', data)
+      modalFrame.innerHTML = filmCard(data);
     })
     .catch(() => {});
 }
 
-imagesRef.addEventListener('click', e => {
+mainCardListEl.addEventListener('click', e => {
   e.preventDefault();
   if (e.target.nodeName !== 'IMG') {
     return;
@@ -23,6 +21,37 @@ imagesRef.addEventListener('click', e => {
 
   id = e.target.dataset.id;
   renderMovieSearchParam(id);
-  modal.classList.remove('hidden');
-  document.body.classList.toggle('modal-open');
+  backdrop.classList.remove('hidden');
+  modalFrame.classList.remove('backdrop-hidden');
+  document.body.classList.add('modal-open');
+  window.addEventListener('keydown', modalCloseByEsc);
+  backdrop.addEventListener('click', backdropClick);
+  closeByButton();
+  listenerModalBtn();
 });
+
+function modalClose() {
+  window.removeEventListener('keydown', modalCloseByEsc);
+  backdrop.removeEventListener('click', backdropClick);
+  backdrop.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  modalFrame.classList.remove('backdrop-hidden');
+  modalFrame.innerHTML = '';
+}
+
+function backdropClick(event) {
+  if (event.currentTarget === event.target) {
+    modalClose();
+  }
+}
+
+function modalCloseByEsc(event) {
+  if (event.code === 'Escape') {
+    modalClose();
+  }
+}
+
+function closeByButton() {
+  const modalCloseBtn = document.querySelector('.close-btn');
+  modalCloseBtn.addEventListener('click', modalClose);
+}
