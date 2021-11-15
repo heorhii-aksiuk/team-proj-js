94a3ea9ae798ec01574747a91f84f141e5ce8ab9
Катерина Осадчая <monzolevskaya@gmail.com>
2021-11-15 21:55:55 +0200

    updata

--- src/js/storage/add-to-queue.js
+++ src/js/storage/add-to-queue.js
@@ -3,6 +3,7 @@ if (localStorageData === null) {
   localStorage.setItem('filmsQueue', JSON.stringify([]));
 }
 export function toggleToQueue(id) {
+  localStorageData = JSON.parse(localStorage.getItem('filmsQueue'));
   if (!localStorageData.includes(id)) {
     localStorageData.push(id);
     localStorage.setItem('filmsQueue', JSON.stringify(localStorageData));
--- src/js/storage/add-to-watched.js
+++ src/js/storage/add-to-watched.js
@@ -4,6 +4,7 @@ if (localStorageData === null) {
   localStorage.setItem('filmsWatched', JSON.stringify([]));
 }
 export function toggleToWatched(id) {
+    localStorageData = JSON.parse(localStorage.getItem('filmsWatched'));
   if (!localStorageData.includes(id)) {
     localStorageData.push(id);
     localStorage.setItem('filmsWatched', JSON.stringify(localStorageData));
