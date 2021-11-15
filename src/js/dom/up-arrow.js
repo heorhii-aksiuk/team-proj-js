4eba112606ad17f0275ab4305569e079f1e5d977
Leopoldovich <levchenkobohdan@gmail.com>
2021-11-15 17:03:30 +0200

    1

---
+++ src/js/dom/up-arrow.js
@@ -0,0 +1,18 @@
+// import from '..partials/up-arrow';
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('up-btn').style.display = 'block';
+  } else {
+    document.getElementById('up-btn').style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
