e90ceaef7dcad0b49058dbefddc254315a2ad071
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-16 23:34:27 +0200

    Genres error fixed

--- src/js/dom/show-all-films.js
+++ src/js/dom/show-all-films.js
@@ -2,6 +2,8 @@ import { fetchAllFilms } from '../api/fetch-all-films';
 import { mainCardListEl } from '../refs';
 import filmCard from '../../templates/film-card.hbs';
 
+const homePage = 1;
+
 export function showAllFilms(page) {
   fetchAllFilms(page).then(createMarkup);
 }
@@ -10,11 +12,12 @@ export function createMarkup(data) {
   const { films, genres } = data;
   const filmsWithGenre = films.map(film => ({
     ...film,
-    genres: genres.filter(genre => film.genre_ids.includes(genre.id)),
+    genres: genres.filter(genre => {
+      if (typeof film.genre_ids !== 'undefined') return film.genre_ids.includes(genre.id);
+    }),
     year: new Date(film.release_date).getFullYear()
   }));
-  // console.log(filmsWithGenre);
   mainCardListEl.innerHTML = filmCard(filmsWithGenre);
 }
 
-showAllFilms(1);
+showAllFilms(homePage);
