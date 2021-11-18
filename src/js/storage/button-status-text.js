08b3d620cadd98edfbcd46dca458b543d5a72caf
Катерина Осадчая <monzolevskaya@gmail.com>
2021-11-18 10:20:44 +0200

    убрала бордер при нажатой кнопке в модалке

--- src/js/storage/button-status-text.js
+++ src/js/storage/button-status-text.js
@@ -13,11 +13,15 @@ export function monitorButtonStatusText(keyStorage, id) {
       addWatched.innerText = filmFromWatched ? 'Delete from watched' : 'Add to watched';
       addWatched.style.backgroundColor = filmFromWatched ? '#FF6B08' : 'white';
       addWatched.style.color = 'black';
+      addWatched.style.borderColor = filmFromWatched ? '#FF6B08' : 'black'
+      
+      
       break;
     case 'filmsQueue':
       addQueue.innerText = filmFromQueue ? 'Delete from queue' : 'Add to queue';
       addQueue.style.backgroundColor = filmFromQueue ? '#FF6B08' : 'white';
       addQueue.style.color = 'black';
+      addQueue.style.borderColor = filmFromQueue ? '#FF6B08' : 'black';
       break;
   }
 }
