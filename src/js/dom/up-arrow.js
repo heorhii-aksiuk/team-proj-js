d2cb9522cea7642e2b208241ab2271d53f83e27f
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-23 19:01:28 +0200

    Show length update

--- src/js/dom/up-arrow.js
+++ src/js/dom/up-arrow.js
@@ -3,7 +3,7 @@ import { upBtnEl } from '../refs';
 window.onscroll = scrollFunction;
 
 function scrollFunction() {
-  upBtnEl.style.display = document.documentElement.scrollTop > 1000 ? 'block' : 'none';
+  upBtnEl.style.display = document.documentElement.scrollTop > 300 ? 'block' : 'none';
 }
 
 upBtnEl.addEventListener('click', () => {
